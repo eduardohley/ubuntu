79266a4b97b74286d1213b88b1c075df8648bbe7
Eduardo Hernández Ley <eduardo.hley@gmail.com>
2022-08-16 19:23:01 -0700

    bashrc

--- setup.sh
+++ setup.sh
@@ -1,3 +1,32 @@
+#!/bin/bash
+
+bash_file="/home/$USER/.bashrc"
+
+WORK_USER=$1
+WORK_IP=$2
+
+if [[ -z "$WORK_USER" ]] || [[ -z "$WORK_IP" ]]
+then
+	echo "params: user ip"
+	exit
+fi
+
+
+cat << EOF >> "/home/$USER/.bashrc"
+
+alias top='bpytop'
+alias vim='nvim'
+alias eb='nvim /home/$USER/.bashrc'
+
+export WORK_USER="$WORK_USER"
+export WORK_IP="$WORK_IP"
+export WORK_SSH="$WORK_USER@$WORK_IP"
+
+alias work='ssh ${WORK_SSH}'
+alias gg='lazygit'
+
+EOF
+
 lazygit_url="https://github.com/jesseduffield/lazygit/releases/download/v0.35/lazygit_0.35_Linux_x86_64.tar.gz"
 neovim_url="https://github.com/neovim/neovim/releases/download/v0.7.2/nvim.appimage"
 bitwarden_url="https://github.com/bitwarden/clients/releases/download/desktop-v2022.8.0/Bitwarden-2022.8.0-x86_64.AppImage"
